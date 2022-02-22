2c4ecb86e216d306e8c99202a665dfaf5ab82ef6
sastubblefield <stubblefield.sa@gmail.com>
2022-02-22 08:59:14 -0800

    form.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
+import Form from "./Form";
 
 function App() {
   return (
     <div className="App">
-      <h1>Dictionary Search</h1>
-      <form>
-        <input className="form-control form-control-sm" type="search" value="ENTER WORD HERE"></input>
-      </form>
-    
+      <h1>Weather Search</h1>
+
+      <Form />
     </div>
   );
 }
---
+++ src/Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function(Form){
+return (
+  <form>
+    <input
+      className="form-control form-control-sm"
+      type="search"
+      value="ENTER WORD HERE"
+    ></input>
+  </form>
+);
+}
